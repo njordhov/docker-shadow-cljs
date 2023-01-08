abd2b40814d368ebe54852c5351c2e69542baad1
Terje Norderhaug <terje@in-progress.com>
2023-01-08 13:02:10 -0800

    Add reference cljs project

---
+++ src/main/script/core.cljs
@@ -0,0 +1,17 @@
+(ns script.core)
+
+(defonce args-var (volatile! nil))
+
+(defn main [& args]
+  (println "=>" args)
+  (vreset! args-var args)
+  args)
+
+(defn ^:dev/before-load loading []
+  (println "\nLoading code changes..."))
+
+(defn ^:dev/after-load activate! [] 
+  (try
+    (tap> (apply main @args-var))
+    (catch :default e 
+      (println e))))
